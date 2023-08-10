ef8fa2091cf321412ee7877add43628177cfee51
benbdeitch <benbdeitch@gmail.com>
2023-08-10 11:43:49 -0400

    fixed uuid

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -7,7 +7,7 @@ export default function MainPage(){
 
     return (
             
-            <Flexbox Items={[{string: "hi", key: uuidv4}, {string: "hello", key: uuidv4}, {string:"How do you do", key:uuidv4}, {string: "342", key:uuidv4}]}/>
+            <Flexbox Items={[{string: "hi", key: uuidv4()}, {string: "hello", key: uuidv4()}, {string:"How do you do", key:uuidv4()}, {string: "342", key:uuidv4()}]}/>
  )
 
 
