4e447d97a45b383917152ac134344798c1621d5c
benbdeitch <benbdeitch@gmail.com>
2023-08-16 17:56:04 -0400

    Finished delete from list functionality

--- src/buttons/RemoveFromListButton.tsx
+++ src/buttons/RemoveFromListButton.tsx
@@ -5,6 +5,7 @@ export default function RemoveFromListButton({string}:encasedString){
     const {user} = useContext(UserContext)
     const buttonOptions= [<button onClick={removeFromList}>Remove from List?</button>, <div className="Box">Item Removed</div>]
     const [buttonState, setButtonState] = useState(buttonOptions[0])
+
     async function removeFromList(){
         let request = {
             method: "POST",
@@ -14,13 +15,18 @@ export default function RemoveFromListButton({string}:encasedString){
             },
             body: JSON.stringify({googleId: string})
         }
-
+        console.log(request)
         const response = await fetch('http://127.0.0.1:5000/api/delete-list', request)
 
         if (response.ok){
             const data = await response.json();
-            alert(data["success"])
+            console.log(data)
+            alert(data["Success"])
             setButtonState(buttonOptions[1])
+        }
+        else{
+            const data = await response.json();
+            console.log(data)
         }
     }
 
