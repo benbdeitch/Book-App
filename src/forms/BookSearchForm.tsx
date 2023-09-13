70bd51656efbacbc44c0f0614c8e830edda0dc18
benbdeitch <benbdeitch@gmail.com>
2023-09-13 18:53:23 -0400

    Continuing pagination work

--- src/forms/BookSearchForm.tsx
+++ src/forms/BookSearchForm.tsx
@@ -2,17 +2,16 @@ import  {useRef, useContext, FormEvent, useEffect, useState } from 'react'
 import {useNavigate} from 'react-router-dom'
 import { UserContext } from '../contexts/UserProvider'
 import { Spinner } from 'react-bootstrap'
-import History_Add_Button from '../buttons/History_Add_Button'
 
-
-import List_Add_Button from '../buttons/List_Add_Button'
 import { levelContext } from '../contexts/UrlProvider'
 import BookRow from '../components/BookRow'
 
 export default function BookSearchForm() {
     const titleField= useRef<HTMLInputElement>(null)
     const authorField = useRef<HTMLInputElement>(null)
-    const entryField = useRef<HTMLInputElement>(null)
+    const [pageNumber, setPageNumber] = useState(1)
+    const author = useRef<string|null>(null)
+    const title = useRef<string|null>(null)
     let { username, token} = useContext(UserContext)
     const navigate = useNavigate()
     const URL  = useContext(levelContext)
@@ -25,27 +24,21 @@ export default function BookSearchForm() {
 
     const [bookData, setBookData] = useState(<></>);
 
-
-
-    async function getBookSearch(e:FormEvent<HTMLFormElement>) {
+    function initializeSearch(e:FormEvent<HTMLFormElement>){
         e.preventDefault()
-        let title:string|null = null; 
-        let author:string|null = null;
-        let howManyEntries:string|null = null;
+        setPageNumber(1)
         if (titleField.current){
-            title = titleField.current.value;
+            title.current = titleField.current.value;
+            console.log(title)
         }
         if (authorField.current){
-            author = authorField.current.value
+            author.current = authorField.current.value
         }
-        if (entryField.current){
-            if (entryField.current.value){
-                howManyEntries = entryField.current.value 
-            }
-            else{
-                 howManyEntries = "10"
-            }
-        }
+        getBookSearch(0)
+    }
+
+    async function getBookSearch(startIndex:number) {
+        
         if (!title &&!author){
             setBookData(<><p className ="Box">Error: Neither title nor author provided.</p></>);
             return ("")
@@ -57,9 +50,9 @@ export default function BookSearchForm() {
                 'Authorization': 'Bearer ' + token
             },
             body: JSON.stringify({
-                'title': title,
-                'author': author,
-                'startIndex': howManyEntries
+                'title': title.current,
+                'author': author.current,
+                'startIndex': startIndex
             })
         }
         console.log(request)
@@ -69,6 +62,10 @@ export default function BookSearchForm() {
         if (response.ok) {
             const data = await response.json();
             console.log(data);
+            let pageButtonMapping:number[] = []
+            for (let i = 0; i<Math.ceil(data["totalItems"]/10); i++){
+                pageButtonMapping.push(i+1)
+            }
             setBookData( 
                 <>
                  <h1 className="Box">Search Page</h1>
@@ -89,6 +86,7 @@ export default function BookSearchForm() {
                        
                     )}
                     </table>
+                    {pageButtonMapping.map((number) => (<button>{number}</button>))}
                 </>)
         } else setBookData(<div className="Box"><h1>No books found.</h1></div>);
         
@@ -102,7 +100,7 @@ export default function BookSearchForm() {
         <>
     <div className = "Box">
         <h2>Book Search Form</h2>
-        <form onSubmit={getBookSearch} className="book-search-form">
+        <form onSubmit={initializeSearch} className="book-search-form">
         <label>
           Title
           <br />
@@ -114,12 +112,6 @@ export default function BookSearchForm() {
           <br />
           <input type="text" ref={authorField} />
         </label><br />
-
-        <label>
-            #Entries:
-            <br />
-            <input type="number" ref={entryField}/>
-        </label>
         <br/>
         <button>Search</button>
       </form>
