85b372db6a0fb457f53e200a7f0af18ee287a3fc
benbdeitch <benbdeitch@gmail.com>
2023-09-15 18:53:01 -0400

    Edited main page instructions

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -6,13 +6,13 @@ export default function MainPage(){
 
     return (
             <div className="Box" >
-                <h1> Welcome to an app for Book Recommendations!</h1>
+                <h1> Welcome to BookApp!</h1>
 
-                <p>Manage your reading list by adding books through the book search. </p>
+                <p> On this website, you can manage a reading list, track your recent reading, and recommend books<br></br> to your friends. </p>
                 <br/>
-                <p>Click on a Book's image, to be taken to a page to recommend the book, or add it to your history.</p>
+                <p>To add a book to your reading list or history, go to Manage List on the Navbar, and select 'Search by Book'. </p>
                 <br/>
-                <p>Search for a user's profile through the search bar. Once you find the profile, you can request to add them as a friend. </p>
+                <p>To access another User's profile, you can search for them in the navigation bar. Once there, you can send a friend request.  </p>
                 <br/>
                 <p>Friends can view each other's reading list and book history. In addition, you can send recommendations for books to add to each other's reading lists. </p>
             </div>
